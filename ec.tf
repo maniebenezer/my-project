58940247c9e82047e3fd5267d1eeead0c59dbdb9
manidevops3694 <manihoney35@gmail.com>
2024-05-01 17:16:20 +0000

    my ec2 terraform code

---
+++ ec.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+        region = "us-east-1"
+}
+
+resource "aws_instance" "my-instance" {
+        ami = "ami-0fe630eb857a6ec83"
+        instance_type = "t2.micro"
+        key_name = "newpvt"
+        tags = {
+                Name = "terraform-ec2"
+        }
+}
+
